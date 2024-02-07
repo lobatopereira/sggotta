67da3b9d3631819a3a125227d61d77fde5688dca
Lourenco Pereira <53714683+lobatopereira@users.noreply.github.com>
2024-02-07 14:51:28 +0900

    07022024_3

--- asggotta/wsgi.py
+++ asggotta/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sggotta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asggotta.settings')
 
 application = get_wsgi_application()
